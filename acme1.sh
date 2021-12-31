5649c305cd5316207c49d84529ecc5e015447124
misakano7545 <misakano7545@protonmail.com>
2021-12-31 19:06:51 +0000

    1

--- acme1.sh
+++ acme1.sh
@@ -1,56 +1,63 @@
 #!/bin/bash
 
+IP=$(curl ipget.net)
+
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
+
 yellow(){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+red(){
+    echo -e "\033[31m$1\033[0m";
+}
+
 if [[ $(id -u) != 0 ]]; then
-yellow " 请以root模式运行脚本。"
-rm -f CFwarp.sh
-exit 0
+    red "请在root用户下运行脚本"
+    exit 0
 fi
 
 if [[ -f /etc/redhat-release ]]; then
-release="Centos"
+    release="Centos"
 elif cat /etc/issue | grep -q -E -i "debian"; then
-release="Debian"
+    release="Debian"
 elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-release="Ubuntu"
+    release="Ubuntu"
 elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-release="Centos"
+    release="Centos"
 elif cat /proc/version | grep -q -E -i "debian"; then
-release="Debian"
+    release="Debian"
 elif cat /proc/version | grep -q -E -i "ubuntu"; then
-release="Ubuntu"
+    release="Ubuntu"
 elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-release="Centos"
+    release="Centos"
 fi	   
 
 function acme(){
-if [ $release = "Centos" ]; then
-yum -y update && yum install curl -y && yum install -y socat 
-else
-apt update -y && apt install curl -y && apt install -y socat
-fi
-curl https://get.acme.sh | sh
-bash /root/.acme.sh/acme.sh --register-account -m xxxx@gmail.com
-read -p "你解析的域名:" ym
-IP=$(curl ipget.net/?ip="$ym")
-yellow "当前的域名IP为：$IP"
-if echo $IP | grep -q ":"; then
-bash /root/.acme.sh/acme.sh  --issue -d ${ym} --standalone -k ec-256 --server letsencrypt --listen-v6
-else
-bash /root/.acme.sh/acme.sh  --issue -d ${ym} --standalone -k ec-256 --server letsencrypt
-fi
-bash /root/.acme.sh/acme.sh --installcert -d ${ym} --key-file /root/private.key --fullchain-file /root/cert.crt --ecc
+    if [ $release = "Centos" ]; then
+        yum -y update && yum install curl -y && yum install -y socat 
+    else
+        apt update -y && apt install curl -y && apt install -y socat
+    fi
+    curl https://get.acme.sh | sh
+    bash /root/.acme.sh/acme.sh --register-account -m xxxx@gmail.com
+    read -p "你解析的域名:" domain
+    domainIP=$(curl ipget.net/?ip="$domain")
+    yellow "VPS本地IP：$IP"
+    yellow "当前的域名解析到的IP：$domainIP"
+    if echo $domainIP | grep -q ":"; then
+        bash /root/.acme.sh/acme.sh  --issue -d ${domain} --standalone -k ec-256 --server letsencrypt --listen-v6
+    else
+        bash /root/.acme.sh/acme.sh  --issue -d ${domain} --standalone -k ec-256 --server letsencrypt
+    fi
+    bash /root/.acme.sh/acme.sh --installcert -d ${domain} --key-file /root/private.key --fullchain-file /root/cert.crt --ecc
 }
 
 function acmer(){
-read -p "你的域名:" ym
-bash /root/.acme.sh/acme.sh --renew -d ${ym} --force --ecc
+    read -p "你的域名:" domain
+    bash /root/.acme.sh/acme.sh --renew -d ${domain} --force --ecc
 }
 
 function start_menu(){
@@ -75,4 +82,4 @@ read -p "请输入数字:" menuNumberInput
     ;;       
  esac
 }   
-start_menu "first" 
+start_menu
