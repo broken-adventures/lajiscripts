f7ce60ea5ef63d9b72b7f5e8664d813923d166ba
misakano7545 <misakano7545@protonmail.com>
2021-12-31 18:27:31 +0000

    1

--- acme1.sh
+++ acme1.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+localIP = `curl ipget.net`
+localIP4 = `curl -4 ipget.net`
+localIP6 = `curl -6 ipget.net`
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+if [[ $(id -u) != 0 ]]; then
+yellow " 请以root模式运行脚本"
+exit 0
+fi
+
+[[ $(type -P yum) ]] && osapt='yum -y' || osapt='apt -y'
+[[ $(type -P curl) ]] || (yellow "curl未安装，正在安装中" && $osapt install curl)
+[[ $(type -P socat) ]] || (yellow "socat未安装，正在安装中" && $osapt install curl)
