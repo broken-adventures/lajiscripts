15b276ad91d83b09a07704902eb0082b2330ebd8
misakano7545 <misakano7545@protonmail.com>
2022-01-08 08:00:26 +0000

    1

--- xrar.sh
+++ xrar.sh
@@ -1,1 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+function install(){
+    bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+}
+
+function makeConfig(){
+    
+}
