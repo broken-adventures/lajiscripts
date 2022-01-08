e977646d530adff6cf20ae6bf4197a58b4a5a980
misakano7545 <misakano7545@protonmail.com>
2022-01-08 01:29:18 +0000

    1

--- acme4.sh
+++ acme4.sh
@@ -1,103 +1,163 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-green(){
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-
-yellow(){
-    echo -e "\033[33m\033[01m$1\033[0m"
-}
-
-red(){
-    echo -e "\033[31m$1\033[0m";
-}
-
-if [[ $(id -u) != 0 ]]; then
-    red "请在root用户下运行脚本"
-    exit 0
-fi
-
+red(){ echo -e "\033[31m\033[01m$1\033[0m";}
+green(){ echo -e "\033[32m\033[01m$1\033[0m";}
+yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
+white(){ echo -e "\033[37m\033[01m$1\033[0m";}
+[[ $EUID -ne 0 ]] && yellow "请以root模式运行脚本" && exit 1
 if [[ -f /etc/redhat-release ]]; then
-    release="Centos"
+release="Centos"
 elif cat /etc/issue | grep -q -E -i "debian"; then
-    release="Debian"
+release="Debian"
 elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-    release="Ubuntu"
+release="Ubuntu"
 elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-    release="Centos"
+release="Centos"
 elif cat /proc/version | grep -q -E -i "debian"; then
-    release="Debian"
+release="Debian"
 elif cat /proc/version | grep -q -E -i "ubuntu"; then
-    release="Ubuntu"
+release="Ubuntu"
 elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-    release="Centos"
+release="Centos"
+else 
+red "不支持你当前系统，请选择使用Ubuntu,Debian,Centos系统" && exit 1
 fi
-[[ $(type -P yum) ]] && yumapt='yum -y' || yumapt='apt -y' 	   
 
-function tls(){
-	bash /root/.acme.sh/acme.sh --installcert -d ${domain} --key-file /root/private.key --fullchain-file /root/cert.crt --ecc
-	[[ -f /root/cert.crt && -f /root/private.key ]] && green "恭喜，tls证书申请成功！域名证书（cert.crt）和私钥（private.key）已保存到 /root 文件夹" || red "遗憾，tls证书申请失败，请查看80端口是否被占用（先lsof -i :80 后kill -9 进程id）或者更换下二级域名称再尝试执行脚本！"
+get_char(){
+SAVEDSTTY=`stty -g`
+stty -echo
+stty cbreak
+dd if=/dev/tty bs=1 count=1 2> /dev/null
+stty -raw
+stty echo
+stty $SAVEDSTTY
 }
 
-function acme() {
-	systemctl stop nginx >/dev/null 2>&1
-	wg-quick down wgcf >/dev/null 2>&1
-	v6=$(curl -s6m3 https://ip.gs -k)
-	v4=$(curl -s4m3 https://ip.gs -k)
-	[[ $(type -P curl) ]] || $yumapt update
-	$yumapt install curl
-	[[ $(type -P socat) ]] || $yumapt install socat
-	curl https://get.acme.sh | sh
-    read -p "请输入需要注册的邮箱：" registerEmail
-	bash /root/.acme.sh/acme.sh --register-account -m $registerEmail
-	read -p "请输入解析完成的域名:" domain
-	green "已输入的域名:$domain"
-	domainIP=$(curl -s ipget.net/?ip="$domain")
-	[[ $domainIP == $v4 ]] && yellow "当前二级域名解析到的IPV4：$domainIP" && bash /root/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --server letsencrypt && tls
-	[[ $domainIP == $v6 ]] && yellow "当前二级域名解析到的IPV6：$domainIP" && bash /root/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --server letsencrypt --listen-v6 && tls
-	[[ -n $(echo $domainIP | grep nginx) ]] && yellow "域名解析无效，请检查二级域名是否填写错误或稍等几分钟等待解析完成再执行脚本"
-	if [[ -n $(echo $domainIP | grep ":") || -n $(echo $domainIP | grep ".") ]]; then
-		if [[ $domainIP != $v4 ]] || [[ $domainIP != $v6 ]]; then
-			red "当前二级域名解析的IP与当前VPS使用的IP不匹配"
-			green "建议如下："
-			yellow "1、请确保Cloudflare小黄云关闭（仅限DNS）状态"
-			yellow "2、请检查域名解析网站设置的IP是否正确"
-		fi
-	fi
-	wg-quick up wgcf >/dev/null 2>&1
-	systemctl start nginx >/dev/null 2>&1
+back(){
+white "------------------------------------------------------------------------------------------------"
+white " 回主菜单，请按任意键"
+white " 退出脚本，请按Ctrl+C"
+exit 0
 }
 
-function renew(){
-    read -p "你的域名:" domain
-    bash /root/.acme.sh/acme.sh --renew -d ${domain} --force --ecc
+checktls(){
+if [[ -f /root/cert.crt && -f /root/private.key ]]; then
+if [[ -s /root/cert.crt && -s /root/private.key ]]; then
+green "恭喜，域名证书申请成功！域名证书（cert.crt）和私钥（private.key）已保存到 /root 文件夹" 
+yellow "证书crt路径如下，可直接复制"
+green "/root/cert.crt"
+yellow "私钥key路径如下，可直接复制"
+green "/root/private.key"
+else
+red "遗憾，域名证书申请失败"
+green "建议如下（按顺序）："
+yellow "1、检测防火墙是否打开"
+yellow "2、请查看80端口是否被占用（先lsof -i :80 后kill -9 进程id）"
+yellow "3、更换下二级域名名称再尝试执行脚本"
+fi
+fi
+}
+acme(){
+systemctl stop nginx >/dev/null 2>&1
+systemctl stop wg-quick@wgcf >/dev/null 2>&1	   
+green "安装必要依赖及acme……"
+[[ $(type -P yum) ]] && yumapt='yum -y' || yumapt='apt -y'
+[[ $(type -P curl) ]] || $yumapt update;$yumapt install curl
+[[ $(type -P socat) ]] || $yumapt install socat
+[[ $(type -P binutils) ]] || $yumapt install binutils
+v6=$(curl -s6m3 https://ip.gs)
+v4=$(curl -s4m3 https://ip.gs)
+auto=`head -n 50 /dev/urandom | sed 's/[^a-z]//g' | strings -n 4 | tr '[:upper:]' '[:lower:]' | head -1`
+curl https://get.acme.sh | sh -s email=$auto@gmail.com
+source ~/.bashrc
+bash /root/.acme.sh/acme.sh --upgrade --auto-upgrade
+yellow "注册acme，创建邮箱的随机前缀：$auto@gmail.com"
+read -p "请输入解析完成的域名:" ym
+green "已输入的域名:$ym" && sleep 1
+domainIP=$(curl -s ipget.net/?ip="cloudflare.1.1.1.1.$ym")
+if [[ -n $(echo $domainIP | grep nginx) ]]; then
+domainIP=$(curl -s ipget.net/?ip="$ym")
+if [[ $domainIP = $v4 ]]; then
+yellow "当前二级域名解析到的IPV4：$domainIP" && sleep 1
+bash /root/.acme.sh/acme.sh  --issue -d ${ym} --standalone -k ec-256 --server letsencrypt
+fi
+if [[ $domainIP = $v6 ]]; then
+yellow "当前二级域名解析到的IPV6：$domainIP" && sleep 1
+bash /root/.acme.sh/acme.sh  --issue -d ${ym} --standalone -k ec-256 --server letsencrypt --listen-v6
+fi
+if [[ -n $(echo $domainIP | grep nginx) ]]; then
+yellow "域名解析无效，请检查二级域名是否填写正确或稍等几分钟等待解析完成再执行脚本"
+back
+elif [[ -n $(echo $domainIP | grep ":") || -n $(echo $domainIP | grep ".") ]]; then
+if [[ $domainIP != $v4 ]] && [[ $domainIP != $v6 ]]; then
+red "当前二级域名解析的IP与当前VPS使用的IP不匹配"
+green "建议如下："
+yellow "1、请确保Cloudflare小黄云关闭状态(仅限DNS)，其他域名解析网站设置同理"
+yellow "2、请检查域名解析网站设置的IP是否正确"
+back
+fi
+fi
+else
+read -p "当前为泛域名申请证书，请复制Cloudflarer的Global API Key:" GAK
+export CF_Key="$GAK"
+read -p "当前为泛域名申请证书，请输入Cloudflarer的登录邮箱地址:" CFemail
+export CF_Email="$CFemail"
+if [[ $domainIP = $v4 ]]; then
+yellow "当前泛域名解析到的IPV4：$domainIP" && sleep 1
+bash /root/.acme.sh/acme.sh --issue --dns dns_cf -d ${ym} -d *.${ym} -k ec-256 --server letsencrypt
+fi
+if [[ $domainIP = $v6 ]]; then
+yellow "当前泛域名解析到的IPV6：$domainIP" && sleep 1
+bash /root/.acme.sh/acme.sh --issue --dns dns_cf -d ${ym} -d *.${ym} -k ec-256 --server letsencrypt --listen-v6
+fi
+fi
+bash /root/.acme.sh/acme.sh --install-cert -d ${ym} --key-file /root/private.key --fullchain-file /root/cert.crt --ecc
+checktls
+systemctl start wg-quick@wgcf >/dev/null 2>&1
+back
 }
 
-function start_menu(){
+Certificate(){
+    [[ -z $(acme.sh -v 2>/dev/null) ]] && yellow "未安装acme.sh证书申请，无法执行" && back
+    bash /root/.acme.sh/acme.sh --list
+    read -p "请输入要撤销并删除的域名证书（复制Main_Domain下显示的域名）:" ym
+    if [[ -n $(bash /root/.acme.sh/acme.sh --list | grep $ym) ]]; then
+        bash /root/.acme.sh/acme.sh --revoke -d ${ym} --ecc
+        bash /root/.acme.sh/acme.sh --remove -d ${ym} --ecc
+        green "撤销并删除${ym}域名证书成功"
+        back
+    else
+        red "未找到你输入的${ym}域名证书，请自行核实！"
+        back
+    fi
+}
+
+acmerenew(){
+    [[ -z $(acme.sh -v) ]] && yellow "未安装acme.sh证书申请，无法执行" && back
+    bash /root/.acme.sh/acme.sh --list
+    read -p "请输入要续期的域名证书（复制Main_Domain下显示的域名）:" ym
+    if [[ -n $(bash /root/.acme.sh/acme.sh --list | grep $ym) ]]; then
+        bash /root/.acme.sh/acme.sh --renew -d ${ym} --force --ecc
+        checktls
+        back
+    else
+        red "未找到你输入的${ym}域名证书，请自行核实！"
+        back
+    fi
+}
+
+start_menu(){
     clear
-    red "=================================="
-    echo "                           "
-    red "    Acme.sh 域名证书一键申请脚本     "
-    red "          by 小御坂的破站           "
-    echo "                           "
-    red "  Site: https://blog.misaka.rest  "
-    echo "                           "
-    red "=================================="
-    echo "                           "
-    echo "                           "
-    echo "1. 申请证书"
-    echo "2. 续期证书"
-    echo "v. 更新脚本"
-    echo "0. 退出脚本"
-    echo "                           "
-    echo "                           "
-    read -p "请输入数字:" menuNumberInput
-    case "$menuNumberInput" in     
-        1 ) acme ;;
-        2 ) renew ;;
-        0 ) exit 0
-    ;;       
+    green " 1.  首次申请证书（自动识别单域名与泛域名） "
+    green " 2.  查询、撤销并删除当前已申请的域名证书 "
+    green " 3.  手动续期域名证书 "
+    green " 0.  退出 "
+    read -p "请输入数字:" NumberInput
+    case "$NumberInput" in     
+        1 ) acme;;
+        2 ) Certificate;;
+        3 ) acmerenew;;
+        0 ) exit 1      
     esac
 }   
-
-start_menu
+start_menu "first" 
