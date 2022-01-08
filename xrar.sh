ebde0cb028a6f86ac9faf5211954742e290fa349
misakano7545 <misakano7545@protonmail.com>
2022-01-08 08:11:26 +0000

    1

--- xrar.sh
+++ xrar.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m";
+}
+
+[[ $EUID -ne 0 ]] && yellow "请以root模式运行脚本" && exit 1
+
 function install(){
     bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
 }
