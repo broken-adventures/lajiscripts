b6ad67d2ac63c81124d3ea4feb681d05bb9ded7c
misakano7545 <misakano7545@protonmail.com>
2022-01-08 09:15:33 +0000

    1

--- xrar.sh
+++ xrar.sh
@@ -13,3 +13,34 @@ function install(){
 function makeConfig(){
     
 }
+
+function uninstall(){
+    xrayr uninstall
+    rm /usr/bin/XrayR -f
+}
+
+function main(){
+        clear
+    red "=================================="
+    echo "                           "
+    red "    Acme.sh 域名证书一键申请脚本     "
+    red "          by 小御坂的破站           "
+    echo "                           "
+    red "  Site: https://blog.misaka.rest  "
+    red " 本脚本在kkkyg的原作上进行二次修改 "
+    echo "                           "
+    red "=================================="
+    echo "                           "
+    green "1. 安装XrayR"
+    green "2. 配置XrayR"
+    green "3. 卸载XrayR"
+    green "0. 退出"
+    echo "         "
+    read -p "请输入数字:" NumberInput
+    case "$NumberInput" in     
+        1 ) install;;
+        2 ) makeConfig;;
+        3 ) uninstall;;
+        0 ) exit 1      
+    esac
+}
