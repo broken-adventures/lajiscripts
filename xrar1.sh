d2ea05e6e3c6cb138629dfa7b48757a539b79260
misakano7545 <misakano7545@protonmail.com>
2022-01-08 09:16:22 +0000

    1

--- xrar1.sh
+++ xrar1.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m";
+}
+
 yellow(){
     echo -e "\033[33m\033[01m$1\033[0m";
 }
 
 [[ $EUID -ne 0 ]] && yellow "请以root模式运行脚本" && exit 1
+
+if [[ -f /etc/redhat-release ]]; then
+    release="Centos"
+elif cat /etc/issue | grep -q -E -i "debian"; then
+    release="Debian"
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+    release="Ubuntu"
+elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+    release="Centos"
+elif cat /proc/version | grep -q -E -i "debian"; then
+    release="Debian"
+elif cat /proc/version | grep -q -E -i "ubuntu"; then
+    release="Ubuntu"
+elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+    release="Centos"
+else 
+    red "不支持你当前系统，请选择使用Ubuntu,Debian,Centos系统" && exit 1    
+fi
 
 function install(){
     bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
