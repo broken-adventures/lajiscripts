622b1fc96f4bd554bad5824afa93cf85b4a93422
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-21 23:18:52 +0800

    Update sys.sh

--- sys.sh
+++ sys.sh
@@ -1,3 +1,9 @@
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "alpine")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+COMPANY=("" "" "" "amazon" "")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update" "apk update -f")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
 	"$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)"
 	"$(lsb_release -sd 2>/dev/null)"
@@ -9,12 +15,6 @@ CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
 for i in "${CMD[@]}"; do
 	SYS="$i" && [[ -n $SYS ]] && echo $SYS && break
 done
-
-REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "alpine")
-RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
-COMPANY=("" "" "" "amazon" "")
-PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update" "apk update -f")
-PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
 
 for ((int=0; int<${#REGEX[@]}; int++)); do
 	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && COMPANY="${COMPANY[int]}" && [[ -n $SYSTEM ]] && break
