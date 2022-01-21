9cb9a892dcc395c2d181bd18d25483f5a81088d3
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-21 23:13:04 +0800

    Update syscheck2.sh

--- syscheck2.sh
+++ syscheck2.sh
@@ -1,10 +1,10 @@
-CMD=(	"$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
 	"$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)"
 	"$(lsb_release -sd 2>/dev/null)"
 	"$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)"
 	"$(grep . /etc/redhat-release 2>/dev/null)"
 	"$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')"
-	)
+)
 
 for i in "${CMD[@]}"; do
 	SYS="$i" && [[ -n $SYS ]] && echo $SYS && break
