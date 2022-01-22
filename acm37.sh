52ddc01276275ed697840ea1222310df264acc78
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-22 12:44:03 +0800

    Update acm37.sh

--- acm37.sh
+++ acm37.sh
@@ -40,17 +40,11 @@ function checkwarp(){
 function checktls(){
     if [[ -f /root/cert.crt && -f /root/private.key ]]; then
         if [[ -s /root/cert.crt && -s /root/private.key ]]; then
-            if [[ -e /etc/wireguard/menu.sh ]]; then
-                warp o
-            fi
             green "证书申请成功！证书（cert.crt）和私钥（private.key）已保存到 /root 文件夹" 
             yellow "证书crt路径如下：/root/cert.crt"
             yellow "私钥key路径如下：/root/private.key"
             exit 0
         else
-            if [[ -e /etc/wireguard/menu.sh ]]; then
-                warp o
-            fi
             red "抱歉，证书申请失败"
             green "建议如下："
             yellow "1. 检测防火墙是否打开"
