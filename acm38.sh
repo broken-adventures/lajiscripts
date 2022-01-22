d8a9eb46c0b07d90ae2d33a036447d17f18811bf
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-22 15:27:13 +0800

    Update acm38.sh

--- acm38.sh
+++ acm38.sh
@@ -34,7 +34,7 @@ done
 for ((int=0; int<${#REGEX[@]}; int++)); do
 	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
 done
-[[ -z $SYSTEM ]] && red "not support" && exit 1
+[[ -z $SYSTEM ]] && red "不支持VPS的当前系统，请使用主流操作系统" && exit 1
 
 function checkwarp(){
     green "检测WARP状态....."
