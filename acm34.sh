9a1cea27ad907787da3599fd12e2d7ad4ecafaa0
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-22 11:03:27 +0800

    Update acm34.sh

--- acm34.sh
+++ acm34.sh
@@ -34,7 +34,7 @@ fi
 
 function checkwarp(){
     green "检测WARP状态....."
-    [[ -n $(wg) ]] && wg-quick down wgcf && yellow "检测到VPS目前已打开WARP，已为你自动关闭WARP以确保证书申请正常执行"
+    [[ -n $(wg) ]] && wg-quick down wgcf && yellow "目前VPS已打开WARP，为了能够正常申请证书，已为你自动关闭WARP以确保证书申请正常执行"
 }
 
 function checktls(){
