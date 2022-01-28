9801d6011b72649aea622e14a63d50fbba6fa888
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-28 17:51:30 +0800

    Update tool42.sh

--- tool42.sh
+++ tool42.sh
@@ -167,6 +167,7 @@ function cyberpanel(){
 
 function qlPanel(){
     docker run -dit --name QL --hostname QL --restart always -p 5700:5700 -v $PWD/QL/config:/ql/config -v $PWD/QL/log:/ql/log -v $PWD/QL/db:/ql/db -v $PWD/QL/scripts:/ql/scripts -v $PWD/QL/jbot:/ql/jbot whyour/qinglong:latest
+    yellow "青龙面板安装成功！！！"
 }
 
 # 第三页
