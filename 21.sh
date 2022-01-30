138403e5a308a5794f3f0631858cf26ba898c979
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-30 20:22:43 +0800

    Create 21.sh

---
+++ 21.sh
@@ -0,0 +1,16 @@
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+read -p "请输入需要设置的root密码:" password
+echo root:$password | sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
+green "用户名：root"
+green "密码：$password"
+yellow "请妥善保存好登录信息！然后重启VPS确保设置已保存！"
