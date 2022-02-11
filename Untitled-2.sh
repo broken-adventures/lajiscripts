e84361a6be3d820fdc246f555c57cf625dea52f1
misakano7545 <misakano7545@protonmail.com>
2022-02-11 10:26:10 +0800

    1

---
+++ Untitled-2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# 控制台字体
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m";
+}
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m";
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m";
+}
+
+# 判断系统及定义系统安装依赖方式
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+ARCH=`uname -m`
+
+# 判断是否为root用户
+[[ $EUID -ne 0 ]] && yellow "请在root用户下运行脚本" && exit 1
+
+# 检测系统，本部分代码感谢fscarmen的指导
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && echo $SYS && break
+done
+
+for ((int=0; int<${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "不支持VPS的当前系统，请使用主流的操作系统" && exit 1
+
+install(){
+    ${PACKAGE_UPDATE[int]}
+    if [ $RELEASE == "CentOS" ]; then
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-${ARCH}.rpm
+        rpm -i cloudflared-linux-${ARCH}.rpm
+    else
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-${ARCH}.deb
+        dpkg -i cloudflared-linux-${ARCH}.deb
+    fi
+}
+
+menu(){
+    echo "1. 安装Argo Tunnel客户端"
+    echo "2. 体验Argo Tunnel隧道"
+    read -p "请输入选项:" menuNumberInput
+    case "$menuNumberInput" in
+        1 ) install ;;
+        0 ) exit 0
+    esac
+}
