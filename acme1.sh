40dc6202528d76df6415d559ac49233b520c2556
misakano7545 <misakano7545@protonmail.com>
2022-02-13 17:18:55 +0800

    1

--- acme1.sh
+++ acme1.sh
@@ -22,7 +22,7 @@ yellow() {
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
 
 for i in "${CMD[@]}"; do
-	SYS="$i" && [[ -n $SYS ]] && echo $SYS && break
+	SYS="$i" && [[ -n $SYS ]] && break
 done
 
 for ((int = 0; int < ${#REGEX[@]}; int++)); do
